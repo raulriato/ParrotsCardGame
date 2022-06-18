755e9ffd0ec8debbcd19046549622babcac534eb
raulriato <raulriato@gmail.com>
2022-06-18 20:19:58 -0300

    feat add game title

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let numberOfCards = prompt('Com quantas cartas você quer jogar? Escolha entre 4 e 14');
 let gifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif']
 
-function isRight(numberOfCards){
+function isInRange(numberOfCards){
     return Number(numberOfCards) >= 4 && Number(numberOfCards) <= 14;
 }
 
@@ -10,7 +10,7 @@ function isEven(numberOfCards){
 }
 
 function isValid(numberOfCards){
-    return isEven(numberOfCards) && isRight(numberOfCards)
+    return isEven(numberOfCards) && isInRange(numberOfCards)
 }
 
 function cardsNumber (){
@@ -60,4 +60,8 @@ function flip(card){
         front.classList.add('hidden');
         back.classList.remove('hidden');
     }
+}
+
+function changeCardImg(){
+    
 }

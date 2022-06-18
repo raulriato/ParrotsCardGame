83bdc0af269625729ebf960e2f683526cf96ee39
raulriato <raulriato@gmail.com>
2022-06-18 18:30:15 -0300

    feat add cards

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let numberOfCards = prompt('Com quantas cartas você quer jogar? Escolha entre 4 e 14');
+let gifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif']
 
 function isRight(numberOfCards){
     return Number(numberOfCards) >= 4 && Number(numberOfCards) <= 14;
@@ -16,13 +17,42 @@ function cardsNumber (){
     while(numberOfCards === null || !isValid(numberOfCards)){
         numberOfCards = prompt('Por favor, digite um número par entre 4 e 14')
     }
-    alert('tudo certo')
+
+    addCards(numberOfCards);
+}
+
+cardsNumber();
+
+function shuffleCards(){
+    return Math.random() - 0.5;
+}
+
+function addCards(numberOfCards){
+    let insertGif = [];
+    for(let i = 0; i < numberOfCards/2; i++){
+        insertGif.push(gifs[i])
+        insertGif.push(gifs[i])
+    }
+    insertGif.sort(shuffleCards);
+    for(let j = 0; j < numberOfCards; j++){
+        cardTemplate = `
+        <li onclick="flip(this)">
+            <img src="./Images/front.png" class="front">
+            <img src="./Images/${insertGif[j]}" class="back hidden">
+        </li>
+        `
+        //console.log(cardTemplate)
+        
+        document.querySelector('ul').innerHTML += cardTemplate
+    }
 }
 
 function flip(card){
     card.classList.toggle('turn');
     front = card.querySelector('.front');
     back = card.querySelector('.back');
+    //console.log(front)
+    //console.log(back)
     if (front.classList.contains('hidden')){
         back.classList.add('hidden');
         front.classList.remove('hidden');

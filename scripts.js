44a46ef023caf57797bafd54cc330477014ddffe
raulriato <raulriato@gmail.com>
2022-06-18 17:06:55 -0300

    feat add prompt

--- scripts.js
+++ scripts.js
@@ -1,7 +1,28 @@
+let numberOfCards = prompt('Com quantas cartas você quer jogar? Escolha entre 4 e 14');
+
+function isRight(numberOfCards){
+    return Number(numberOfCards) >= 4 && Number(numberOfCards) <= 14;
+}
+
+function isEven(numberOfCards){
+    return Number(numberOfCards)%2 === 0;
+}
+
+function isValid(numberOfCards){
+    return isEven(numberOfCards) && isRight(numberOfCards)
+}
+
+function cardsNumber (){
+    while(numberOfCards === null || !isValid(numberOfCards)){
+        numberOfCards = prompt('Por favor, digite um número par entre 4 e 14')
+    }
+    alert('tudo certo')
+}
+
 function flip(card){
     card.classList.toggle('turn');
-    front = document.querySelector('.front');
-    back = document.querySelector('.back');
+    front = card.querySelector('.front');
+    back = card.querySelector('.back');
     if (front.classList.contains('hidden')){
         back.classList.add('hidden');
         front.classList.remove('hidden');
